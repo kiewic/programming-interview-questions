69f72a2f816cc40fba9ac165f11aec790d2ceb8d
Gilberto Stankiewicz <kiewic@gmail.com>
2017-01-08 21:16:46 -0800

    Add Progress Pie solution

--- HackerCup2017/ProgressPie/Source.cpp
+++ HackerCup2017/ProgressPie/Source.cpp
@@ -7,29 +7,6 @@
 
 char screen[101][101];
 
-char pythagoras(double x, double y)
-{
-    if (std::sqrt(std::pow(x - 50, 2) + std::pow(y - 50, 2)) > 50)
-    {
-        return '.';
-    }
-    return '+';
-}
-
-void printScreen()
-{
-    // Only for debugging purposes.
-    for (int i = 0; i < 101; i++)
-    {
-        for (int j = 0; j <= 101; j++)
-        {
-            screen[i][j] = pythagoras(i, j);
-            printf("%c", screen[i][j]);
-        }
-        printf("\n");
-    }
-}
-
 double getRadio(double x, double y)
 {
     double r = std::sqrt(std::pow(x - 50, 2) + std::pow(y - 50, 2));
@@ -44,9 +21,17 @@ double getAngle(double x, double y)
 
     double atan = std::atan(m) * 180.0 / 3.141592653589793;
     double angle = 400.0; // error
-    if ((m < 0 && xs > 0) || (atan > 0))
+    if (ys == 0 && xs > 0)
     {
-        if (ys > 0 && xs > 0)
+        angle = 90;
+    }
+    else if (ys < 0 && xs == 0)
+    {
+        angle = 180;
+    }
+    else if ((m < 0 && xs > 0) || (atan > 0))
+    {
+        if ((ys > 0 && xs > 0))
         {
             angle = atan;
         }
@@ -62,24 +47,56 @@ double getAngle(double x, double y)
     return angle;
 }
 
-
 double getPercent(double x, double y) {
     double p = getAngle(x, y) / 360.0;
     return p;
 }
 
+bool test(int p, int x, int y)
+{
+    if (getRadio(x, y) > 50 || getPercent(x, y) > p / 100.0)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+char pythagoras(int x, int y)
+{
+    if (test(99, y, x))
+    {
+        return '.';
+    }
+    return 'O';
+}
+
+void printScreen()
+{
+    // Only for debugging purposes.
+    for (int i = 100; i >= 0; i--)
+    {
+        for (int j = 0; j <= 100; j++)
+        {
+            screen[i][j] = pythagoras(i, j);
+            printf("%c", screen[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main()
 {
+    //printScreen();
+
     int t;
     scanf("%d", &t);
-
     for (int i = 1; i <= t; i++)
     {
-        int p = 13;
-        int x = 55, y = 55;
+        int p = 13, x = 55, y = 55;
         scanf("%d %d %d", &p, &x, &y);
 
-        if (getRadio(x, y) > 50 || getPercent(x, y) > p / 100.0)
+        if (test(p, x, y))
         {
             printf("Case #%d: white\n", i);
         }
@@ -88,8 +105,6 @@ int main()
             printf("Case #%d: black\n", i);
         }
     }
-
-    //printScreen();
 
     return 0;
 }
